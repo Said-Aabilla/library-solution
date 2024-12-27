e832d7d0a80def51eb463b458dbfe51b8ffcd536
Aabilla Said <said160aabilla@gmail.com>
2024-12-27 18:05:25 +0100

    Agregation: Book and Author

--- Author.php
+++ Author.php
@@ -1,21 +1,50 @@
 <?php
 
+require_once "Book.php";
 
 class Author
 {
-    public string $name;
-    public $books = [];
+    private string $name;
+    private $books = [];
+
 
     // TODO Generate getters and setters of properties
-    // TODO Generate constructor for all attributes. $books argument of the constructor can be optional
+
+    public function setName($name){
+        $this->name = $name;
+    }
+ 
+    public function setBooks($books){
+        $this->books = $books;
+    }
+
+
+    public function getName(){
+        return $this->name ;
+    }
+
+    public function getBooks(){
+       return  $this->books ;
+    }
+
+    // TODO Generate constructor for all attributes. 
+    //$books argument of the constructor can be optional
+
+    public function __construct($name, $books = []){
+        $this->name = $name;
+        $this->books = $books;
+    }
 
     /**
      * @param string $title
      * @param float  $price
      * @return \Book
      */
-    public function addBook(string $title, float $price): Book
+    public function addBook(string $title, float $price) : Book
     {
-        // TODO Create instance of the book. Add into $books array and return it
+        $book = new Book($title, $price);
+        //Add into $books array and return it
+        $this->books[] = $book; //books.push($book)
+        return $book;
     }
 }
--- Book.php
+++ Book.php
@@ -1,12 +1,50 @@
 <?php
+require_once "Author.php";
 
 
 class Book
 {
-    public string $title;
-    public float $price;
-    public Author $author;
+    private string $title;
+    private float $price;
+    private Author $author;
 
     // TODO Generate getters and setters of properties
-    // TODO Generate constructor for all attributes. $author argument of the constructor can be optional
+
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+
+    public function getPrice(){
+        return $this->price;
+    }
+
+
+    public function getAuthor(){
+        return $this->author;
+    }
+
+
+    public function setTitle($title){
+         $this->title = $title;
+    }
+
+
+    public function setPrice($price){
+         $this->price = $price;
+    }
+
+
+    public function setAuthor($author){
+         $this->author = $author;
+    }
+
+    // TODO Generate constructor for all attributes. 
+    //$author argument of the constructor can be optional
+    public function __construct($title, $price, $author=null){
+        $this->title = $title;
+        $this->price = $price;
+        $this->author = $author;
+    }
 }
